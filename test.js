29bcbaee7d6ec7b19f7ab4f82ea1b076cad03b6d
sextynine <mohammad87115@gmail.com>
2024-12-24 11:06:25 +0330

    Remove test.js file as it is no longer needed

--- test.js
+++
@@ -1,3 +0,0 @@
-const compare = require('./functions/compare.js')
-
-const a = compare.compareWithFolderImages("https://media.discordapp.net/attachments/1301284845586092093/1321009815736025261/nt_tOfczdcbcethnYO.png?ex=676badc3&is=676a5c43&hm=798cbfd6718a7b105375e2fa422a52d291a9f4e5c32693acc84191e85e347a9b&=&format=webp&quality=lossless&width=410&height=545")
